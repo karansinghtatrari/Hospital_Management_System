275c4e32439a764d78c4e044f92721c11ccdb21f
karansinghtatrari <karansinghtatrari@gmail.com>
2026-09-24 00:32:59 +0530

    Update Patient.java

--- Patient.java
+++ Patient.java
@@ -1,28 +1,39 @@
 package HospitalManagement;
+
 public class Patient {
-	private static int IdCounter=1;
-	private int Id;
-	private String name;
-	private String gender;
-	private int age;
-	
-	public Patient(String name,String gender,int age) {
-		this.Id=IdCounter++;
-		this.name=name;
-		this.gender=gender;
-		this.age=age;
-	}
-	
-	public String toString() {
-		return "Patient ID:"+Id+", Name:"+name+",Gender:"+gender+",Age:"+age;
-	}
-	public int getId() {
-		return Id;
-	}
-	public String getname() {
-		return name;         
-	}
-	public int getage() {
-		return age;
-	}
-}  
+    private static int idCounter = 1;
+
+    private final int id;
+    private final String name;
+    private final String gender;
+    private final int age;
+
+    public Patient(String name, String gender, int age) {
+        this.id = idCounter++;
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "Patient ID: " + id + ", Name: " + name + ", Gender: " + gender + ", Age: " + age;
+    }
+}
+	 
